a627b62947c32acf6985ddd73f7d2b40eaaf96ed
Erik Harding <erik@praekelt.org>
2022-02-24 08:21:59 +0200

    Remove access token config

--- config.py
+++ config.py
@@ -5,5 +5,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     TEMPLATE_URL = os.environ.get("TEMPLATE_URL")
-    ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
     FB_BUSINESS_ID = os.environ.get("FB_BUSINESS_ID", "27796312456")
